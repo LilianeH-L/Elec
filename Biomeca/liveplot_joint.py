1319b13c146c4d14a88069107b910495068b5468
Hugues Martin <huguesgmartin1@gmail.com>
2019-11-13 22:00:15 -0500

    Changement des fichiers liveplot_joint, plus beau, prompt

--- Biomeca/liveplot_joint.py
+++ Biomeca/liveplot_joint.py
@@ -1,23 +1,25 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 
 # use ggplot style for more sophisticated visuals
 plt.style.use('ggplot')
 
-def live_plotter_angle(ax,line1,angleCoude,angleEpaule,T1,style,a,identifier='',pause_time=1):
+def live_plotter_angle(ax,line1,angleCoude,angleEpaule,T1, T2,style,a,identifier='',pause_time=1):
     #position en x
-    x=1
+    x=0.75
     #position en y
-    y=1
+    y=0.75
     #Évaluer la position du bout de la ligne avec la trigonométrie
+    #vecteur xx et yy: [position de l'épaule, position du coude, position du poignet]
     phiE = np.radians(angleEpaule)
-    phiC = np.radians((90-angleCoude+angleEpaule))
-    xx = [x, x+.5*np.sin(phiE), x + .5*(np.cos(phiC)+np.sin(phiE))]
-    yy = [y+.5, y+.5-.5*np.cos(phiE), y +.5 +.5*np.sin(phiC)-.5*np.cos(phiE)]
-    
-    
+    #phiC = np.radians((90-angleCoude+angleEpaule))
+    phiC=np.radians(angleCoude)
+    #xx = [x, x+.5*np.sin(phiE), x + .5*(np.cos(phiC)+np.sin(phiE))]
+    xx = [x, x+.5*np.sin(phiE), x + .5*(np.sin(phiC-phiE)+np.sin(phiE))]
+    #yy = [y+.5, y+.5-.5*np.cos(phiE), y +.5 +.5*np.sin(phiC)-.5*np.cos(phiE)]
+    yy = [y+.5, y+.5-.5*np.cos(phiE), y +.5 +.5*np.cos(phiC-phiE)-.5*np.cos(phiE)]
    
     if line1==[]: 
         # this is the call to matplotlib that allows dynamic plotting
@@ -32,8 +34,7 @@ def live_plotter_angle(ax,line1,angleCoude,angleEpaule,T1,style,a,identifier='',
             line1, = ax.plot(xx[1], yy[1], 'o', color='tab:red', markersize=3)
         
         #update plot label/title
-        plt.ylabel('Y Label')
-        plt.title('Title: {}'.format(identifier))
+        plt.title(input('entrez le mouvement: '))
         plt.show()
       
 
@@ -43,12 +44,13 @@ def live_plotter_angle(ax,line1,angleCoude,angleEpaule,T1,style,a,identifier='',
     # update x, y et le text
     line1.set_xdata(xx)
     line1.set_ydata(yy)
-    T1.set_text( f'{angleCoude} degrees')
+    T1.set_text( f'Coude: {angleCoude} degrés')
+    T2.set_text(f'Épaule: {angleEpaule} degrés')
     # limite la grosseur du graph
     plt.xlim(0,2)
     plt.ylim(0,2)
-    ax.xaxis.set_visible(True)
-    ax.yaxis.set_visible(True)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
     # Pause ( peut être inutile)
     plt.pause(pause_time)
     
--- Biomeca/liveplot_jointtest.py
+++ Biomeca/liveplot_jointtest.py
@@ -10,14 +10,15 @@ line3 = []
 #construire le subplots appelé ax
 fig, ax = plt.subplots(figsize=(8,6))
 #définir l'emplacement du texte de l'angle
-T1=ax.text(0.3,1,'')
+T1=ax.text(0.2,1,'')
+T2=ax.text(0.2, 0.8, '')
 while True:
     style='round'
     #input des valeurs de 0 à 180 afin de tester
-    angleCoude=90
     angleEpaule=30
     #grosses lignes bleues
-    line1 = live_plotter_angle(ax,line1,angleCoude,angleEpaule,T1,style,a=1,identifier='',pause_time=0.1)
+    for angleCoude in range(0,180):
+        line1 = live_plotter_angle(ax,line1,angleCoude,angleEpaule,T1, T2,style,a=1,identifier='position du bras',pause_time=0.01)
     #traits fins noirs
     #line2 = live_plotter_angle(ax,line2,angle,T1,style,a=2,identifier='',pause_time=0.05)
     #points rouges
