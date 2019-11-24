86876817bab64d8593eb1ec567b5ffb3f55943d5
Nathan Girard <nathan.girard@polymtl.ca>
2019-11-24 12:00:19 -0500

    Ecriture et lecture des donnees (Teensy et SerialCom) - manque juste le temps à ajouter dans SerialCom

--- Biomeca/lecture_encodeurs/lecture_encodeurs.ino
+++ Biomeca/lecture_encodeurs/lecture_encodeurs.ino
@@ -10,8 +10,9 @@
 //   Best Performance: both pins have interrupt capability
 //   Good Performance: only the first pin has interrupt capability
 //   Low Performance:  neither pin has interrupt capability
-Encoder knobLeft(14, 15);
-//Encoder knobRight(7, 8);
+Encoder knob1(14, 15);
+Encoder knob2(7, 8);
+Encoder knob3(2, 3);
 //   avoid using pins with LEDs attached
 
 void setup() {
@@ -19,40 +20,66 @@ void setup() {
   Serial.println("TwoKnobs Encoder Test:");
 }
 
-long positionLeft  = -999;
-//long positionRight = -999;
-bool debut = true;
-unsigned long t_debut=0;
+long position1  = -999;
+long position2 = -999;
+long position3 = -999;
+//bool debut = true;
+//unsigned long t_debut = 0;
+//unsigned long pos = 0;
+
 void loop() {
-  long newLeft; //, newRight;
-  newLeft = knobLeft.read()*360/1600; // 1600 pics/tour convertis en degrés
-  //newRight = knobRight.read();
-  if (newLeft > 360)
-    newLeft = newLeft%360; //modulo 360
-  else if (newLeft < 0)
-    newLeft = newLeft%360 + 360;
-  if (newLeft != positionLeft) { //|| newRight != positionRight) {
-    if (debut == true) {
-      t_debut = millis();
-      debut = false;
-      }
-    unsigned long temps = millis() - t_debut;
-    Serial.print("Left = ");
-    Serial.print(newLeft);
-    //Serial.print(", Right = ");
-    //Serial.print(newRight);
-    Serial.print(" Time = ");
-    Serial.print(temps);
-    Serial.println();
-    positionLeft = newLeft;
-    //positionRight = newRight;
+  long new1, new2, new3;
+  new1 = knob1.read()*360/1600; // 1600 pics/tour convertis en degrés
+  new2 = knob2.read();
+  new3 = knob3.read();
+  
+  if (new1 > 360){
+    new1 = new1%360; //modulo 360
+    }
+  else if (new1 < 0){
+    new1 = new1%360 + 360;
   }
+  if (new2 > 360){
+    new2 = new2%360; //modulo 360
+  }
+  else if (new2 < 0){
+    new2 = new2%360 + 360;
+    }
+  if (new3 > 360){
+    new3 = new3%360; //modulo 360
+  }
+  else if (new3 < 0){
+    new3 = new3%360 + 360;
+    }
+  
+    
+  if (new1 != position1 || new2 != position2 || new3 != position3) {
+//    if (debut == true) {
+//      t_debut = millis();
+//      debut = false;
+//      }
+//    unsigned long temps = millis() - t_debut;
+    Serial.print('#');
+//    Serial.print("1 = ");
+    Serial.print(new1);
+//    //Serial.print(", 2 = ");
+    Serial.print(new2);
+    Serial.print(new3);
+//    Serial.print(" Time = ");
+//    Serial.print(temps);
+//    Serial.println();
+    position1 = new1;
+    position2 = new2;
+    position3 = new3;
+  }
+  
   // if a character is sent from the serial monitor,
-  // reset both back to zero.
+  // send a signal to indicate the end of the measure
   if (Serial.available()) {
     Serial.read();
-    Serial.println("Reset both knobs to zero");
-    knobLeft.write(0);
-    //knobRight.write(0);
+    Serial.print('%');
+    knob1.write(0);
+    knob2.write(0);
+    knob3.write(0);
   }
 }
--- Biomeca/serialCom2.py
+++ Biomeca/serialCom2.py
@@ -5,55 +5,40 @@ import threading
 import os
 import math
 												#Commencer par le main pour mieux comprendre
-kNElectrodes = 1 # on garde --> nombre d'encodeurs
-kSizePacket = 350 # inutile
-kNSamplesPerPacket = int(kSizePacket / 2) # inutile
-kSamplePerSecond = 10 # inutile --> ils s'en servent seulement pour la taille du tableau
-
+kNEncodeurs = 1 
 
 #info sur ce qu'on veut mesurer, combien de temps, utile pour stocker
-print("Temps d'acquisition?")
-kAcquisitionTime = input() # à changer par une commande de début/fin dans le terminal
-
 print("Classe de mouvement?")
-movement_class = input() # on garde
+movement_class = input() 
 
 print("Numéro d'acquisition?")
-acquisition_number = input() # on garde
+acquisition_number = input() 
 
-#jusqu'au numéro de packets à prendre (n'est pas défini dans le code, à corriger) va enregistrer 
-#dans un array nommé emg_data et le sauvegarder en format binary, va ensuite sortir
-#
+
 def process_serial_buffer(q):
-    electrode = q.get()
-    processed_packets = 0
-    #np.zeros crée un array de 0, de taille kNElectrodes x kNumPacketsToAcquire * kNSamplesPerPacket / kNElectrodes
-    emg_data = np.zeros((kNElectrodes, int(int(kAcquisitionTime)*kSamplePerSecond / kNElectrodes)), dtype=int)
-    while True:
-        packet = np.zeros(kSamplePerSecond, dtype=int)
-        for i in range(0, kSizePacket, 2):
-            low = q.get()
-            high = q.get()
-            sample = low + (high << 8) #Sample=donnee lue
-            packet[int(i / 2)] = sample #Packet=ensemble de samples pour une electrode (dans notre cas ce sera plutot un encodeur)
-
-			#Information est lue par increments de 8 bytes mais la donnee est de 16 bytes, 
-			#donc il faut shift la deuxieme lecture pour additionner correctement et obtenir une sample finale de 16 bytes dans le packet, 
-			#reste a determiner combien de bytes sera la donnee du Teensy dans notre cas
-
-
-        start_index = int(processed_packets / kNElectrodes) * kNSamplesPerPacket
-        emg_data[electrode, start_index: start_index + kNSamplesPerPacket] = packet #Les donnees d un packet (ensemble de samples pour une electrode donnee) sont stockees dans
-        electrode = electrode + 1													#emg_data qui regroupe l ensemble des donnees et l electrode a laquelle elles correspondent
-        electrode = electrode % kNElectrodes  
-        processed_packets = processed_packets + 1
-        if processed_packets >= kNumPacketsToAcquire: #Si on a toutes les donnees desirees on save, sinon on recommence la boucle pour une nouvelle electrode
-            np.save(movement_class + "_" + str(kAcquisitionTime) + "s_" + str(kSamplePerSecond) + "Hz_" +
-                    acquisition_number, emg_data) #enregistre les données du tableau emg_data dans un fichier .npy
-                                                #du style abduction_3s_200Hz_1.npy 
+    angles_data = np.zeros((1, kNEncodeurs+1), dtype=int)
+    while True: 
+        # on lit le premier caractère (16 bits)
+        low = q.get()
+        high = q.get()
+        sample = low + (high << 8) 
+        if sample == '#': # si c'est un #, ça indique le début d'une position
+                position = np.zeros((kNEncodeurs+1,), dtype=int)
+                for i in range(0,kNEncodeurs-1): # on enregistre chaque angle (et le temps ?) dans une colonne de position
+                    low = q.get()
+                    high = q.get()
+                    sample = low + (high << 8) 
+                    position [i] = sample
+                if angles_data.shape[0] == 1: # si c'est la première position, angles_data=position
+                    angles_data = position
+                else:
+                    angles_data.append(position) # sinon on ajoute position à la fin d'angles_data
+        if sample == '%': # si on lit %, ça indique que la mesure est terminée
+            np.save(movement_class + "_" + acquisition_number, angles_data) # on enregistre les données
+                                                                            # au format "abduction_1.npy" 
             os._exit(0)
 
-
+# Rien à changer dans main
 def main():
     port_open = False	#Ouverture du port du Teensy pour debuter la lecture
     while not port_open:
@@ -65,9 +50,9 @@ def main():
         except:
             pass
 
-	#Le code a deux unites fonctionnelles qui travaillent en parallele : la boucle while juste en dessous recupere les donnes et les mets en "file d attente" dans la variable q 
+	#Le code a deux unites fonctionnelles qui travaillent en parallele : la boucle while juste en dessous recupere les donnes et les met en "file d attente" dans la variable q 
 	#La variable q est donc un buffer qui fait le pont entre les deux unites. La fonction process_serial_buffer est appelee avec consumer et prend la variable q en argument 
-	#et sauve les valeurs lues sous forme de packet associe a une electrode le tout organise dans un tableau (np.save). Le buffer est donc ecrit et lu simultanement.
+	#et sauve les valeurs lues dans un tableau (np.save). Le buffer est donc ecrit et lu simultanement.
     q = queue.Queue() 
     consumer = threading.Thread(target=process_serial_buffer, args=(q,))
     consumer.start()
