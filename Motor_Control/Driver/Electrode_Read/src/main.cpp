3b364cab8cc0f6838a2ea8887f317877446e7a3e
Charles <charles.sirois14@gmail.com>
2019-12-01 20:59:42 -0500

    Demo 2

--- Motor_Control/Driver/Electrode_Read/src/main.cpp
+++ Motor_Control/Driver/Electrode_Read/src/main.cpp
@@ -5,26 +5,31 @@
  */
 
 #include <Arduino.h>
-
-int upcnt = 0;
-int downcnt = 0;
-int delay_time = 10;
-bool up = false;
-
-
-int upThres = 250;
-int upCountThres = 500;
-
-int downThres = 550;
-int downCountThres = 500;
-
 int comPin = 18;
 int biceps;
+int delay_time = 10;
+
+bool up = false;
+int upcnt = 0;
+int upThres = 275;
+int upCountThres = 200;
+int upDelay = 1500;
+
+int downcnt = 0;
+int downThres = 800;
+int downCountMaxThres = 30;
+int downDelay = 2500;
+int downCountThres = 750;
+
+int maxCount = 0;
+bool maxReached = false;
+int maxThres = 950;
+int maxCountThres = 300;
 
 void setup() {
   pinMode(13, OUTPUT);
   pinMode(comPin, OUTPUT);
-  digitalWriteFast(13, HIGH);
+  digitalWriteFast(13, up);
   delay(1000);
 }
 
@@ -33,27 +38,55 @@ void loop() {
 
   biceps = analogRead(15);
   //auto triceps = analogRead(16);
+
   Serial.print(biceps);
   Serial.print("\t");
   //Serial.println(triceps);
 
+  // Conditions to lower
   if (up)
   {
-
     Serial.println("up");
     if (biceps < downThres)
       downcnt++;
     else 
       downcnt = 0;
 
-    if (downcnt > downCountThres / delay_time)
+    
+    //Check max has been reached
+    if(biceps > maxThres)
+      maxCount++;
+    else
+      maxCount = 0;
+
+    if (maxCount > maxCountThres / delay_time)
+      maxReached = true;
+
+
+    if (downcnt>downCountMaxThres/delay_time && maxReached)
     {
+      downcnt = 0;
+      maxCount = 0;
       up = false;
+      maxReached = false;
+      digitalWrite(comPin, up);
+      digitalWrite(13, up);
+      delay(downDelay);
+    }
+    
+    if(downcnt > downCountThres / delay_time){
+      downcnt = 0;
+      maxCount = 0;
+      up = false;
+      maxReached = false;
       digitalWrite(comPin, up);
       digitalWrite(13, up);
     }
-    
+
+
   }
+
+  // Conditions to go up
   else
   {
     Serial.println("down");
@@ -68,7 +101,8 @@ void loop() {
       up = true;
       digitalWrite(comPin, up);
       digitalWrite(13, up);
-    }
+      delay(upDelay);
+    }    
   }
   delay(delay_time);
 }
