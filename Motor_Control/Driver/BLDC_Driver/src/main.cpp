e2608b56ad36cd82ba52837286141664ac710927
aicha <miloudi.ai04@gmail.com>
2020-03-12 18:45:01 -0400

    working on general test

--- Motor_Control/Driver/BLDC_Driver/src/main.cpp
+++ Motor_Control/Driver/BLDC_Driver/src/main.cpp
@@ -21,17 +21,18 @@ const int maximumSpeed = 2048;
 
 double Setpoint, Input, Output;
 // Tuning parameters
-double Kp=4, Kd=30, Ki=0;
+double Kp = 4, Kd = 30, Ki = 0;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, REVERSE);
 
 void setup()
 {
   Serial.begin(9600);
-  while(!Serial.available());
+  while (!Serial.available())
+    ;
   Serial.println("Starting");
   SPISetup();
   motorSetup();
-  
+
   Setpoint = 0;
 
   //tell the PID to range between 0 and the full window size
@@ -46,24 +47,5 @@ void setup()
 
 void loop()
 {
-  int dir = 0;
-  double actualAngle = readAngle();
-  Serial.print("angle: ");
-  Serial.print(actualAngle);
-
-  double error = 360 - actualAngle;
-  
-  dir = error > 0 ? 0 : 1;
-  
-  Input = error > 0 ? error : -1 * error;
-  
-  Serial.print("\t dir: ");
-  Serial.print(dir);
-  Serial.print("\t input: ");
-  Serial.print(Input);
-  
-  myPID.Compute();
-  turnMotor(Output, dir);
-  Serial.print("\t output: ");
-  Serial.println(Output);
+  generalTest();
 }
